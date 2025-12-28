f8712257a509d2b161ff115eed85a87b8120b8cd
rnersesian <nersesian.robert@gmail.com>
2025-12-28 15:47:33 +0100

    load images before show anything

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+function loadResources()
+{
+    const bgImage = new Image();
+    bgImage.src = 'resources/images/conference-background.svg';
+
+    bgImage.onload = () => {
+        document.body.classList.remove('loading');
+    };
+
+    // Fallback
+    setTimeout(() => {
+        document.body.classList.remove('loading');
+    }, 3000);
+}
+
+window.addEventListener('load', () => {
+    loadResources()
+});
+
+
+//
+// User events
+//
+
 function showSideBar()
 {
     const sidebar = document.querySelector('.sidebar')
