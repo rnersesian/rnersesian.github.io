5180a4573cbb890412c1fcc03df4e26abfa8893d
rnersesian <nersesian.robert@gmail.com>
2025-12-28 14:26:31 +0100

    sections

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 function showSideBar()
 {
     const sidebar = document.querySelector('.sidebar')
+    const menuIcon = document.querySelector('nav #menu-icon')
+
     sidebar.style.display = "flex"
+    menuIcon.style.visibility = "hidden"
 }
 
 function hideSideBar()
 {
     const sidebar = document.querySelector('.sidebar')
+    const menuIcon = document.querySelector('nav #menu-icon')
+
     sidebar.style.display = "none"
+    menuIcon.style.visibility = "visible"
 }
